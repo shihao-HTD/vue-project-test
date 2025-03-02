fdb5e476f1f322a8fe111a2212faeb9e9ee0b7c8
独断万古、荒 <411496621@qq.com>
2025-03-02 20:41:44 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <HelloWorld></HelloWorld>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<style lang="scss" scoped>
+
+</style>
